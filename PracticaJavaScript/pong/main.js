035839b05442ed4d583019f900f34436d49df203
SebasMS <smejias1996@gmail.com>
2021-11-09 17:44:47 -0500

    Darle movimiento a la pelota y asignar el evento pausa a la tecla espaciadora

--- PracticaJavaScript/pong/main.js
+++ PracticaJavaScript/pong/main.js
@@ -6,12 +6,13 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements(){
-            var elements = this.bars;
-            //elements.push(this.ball);
+            var elements = this.bars.map(function(bar){ return bar; });
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -25,8 +26,17 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
+
         board.ball = this;
         this.kind = "circle";
+    }
+
+    self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
     }
 })();
 
@@ -77,8 +87,11 @@
             };
         },
         play: function(){
-            this.clean();
-            this.draw();
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -106,22 +119,29 @@ var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function(event){
-    event.preventDefault();
-
     if(event.keyCode === 38){
+        event.preventDefault();
         bar.up();
     }
     else if(event.keyCode === 40){
+        event.preventDefault();
         bar.down();
     }
     else if(event.keyCode === 87){ // W
+        event.preventDefault();
         bar_2.up();
     }
     else if(event.keyCode === 83){ // S
+        event.preventDefault();
         bar_2.down();
+    }
+    else if(event.keyCode === 32){ // PAUSE
+        event.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
+board_view.draw();
 window.requestAnimationFrame(controller);
 
 function controller() {
