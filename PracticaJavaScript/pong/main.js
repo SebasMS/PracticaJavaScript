80701b502fa96b20c12ece7e85f563ca4d718fa0
SebasMS <smejias1996@gmail.com>
2021-11-09 17:25:26 -0500

    Diseñando el balón de ping pong

--- PracticaJavaScript/pong/main.js
+++ PracticaJavaScript/pong/main.js
@@ -11,9 +11,22 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(this.ball);
+            //elements.push(this.ball);
             return elements;
         }
+    }
+})();
+
+(function(){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
     }
 })();
 
@@ -26,7 +39,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed = 10;
+        this.speed = 5;
     }
 
     self.Bar.prototype = {
@@ -53,46 +66,65 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.context.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function(){
             for(var i = this.board.elements.length - 1; i >= 0; i--){
-                var elmt = this.board.elements[i];
+                var element = this.board.elements[i];
 
-                draw(this.context, elmt);
-            }
+                draw(this.context, element);
+            };
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(context, element){
-        if(element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "rectangle":
-                    context.fillRect(element.x, element.y, element.width, element.height);
-                    break;
-            }
+        switch(element.kind){
+            case "rectangle":
+                context.fillRect(element.x, element.y, element.width, element.height);
+                break;
+
+            case "circle":
+                context.beginPath();
+                context.arc(element.x, element.y, element.radius, 0, 7);
+                context.fill();
+                context.closePath();
+                break;
         }
     }
 })();
 
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar_2 = new Bar(740, 100, 40, 100, board);
+var canvas = document.getElementById('canvas', board);
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
 document.addEventListener("keydown", function(event){
-    if(event.keyCode == 38){
+    event.preventDefault();
+
+    if(event.keyCode === 38){
         bar.up();
     }
-
-    else if(event.keyCode == 40){
+    else if(event.keyCode === 40){
         bar.down();
+    }
+    else if(event.keyCode === 87){ // W
+        bar_2.up();
+    }
+    else if(event.keyCode === 83){ // S
+        bar_2.down();
     }
 });
 
-self.addEventListener("load", main);
+window.requestAnimationFrame(controller);
 
-function main() {
-    var board = new Board(800, 400);
-
-    var bar = new Bar(20, 100, 40, 100, board);
-    var bar = new Bar(740, 100, 40, 100, board);
-
-    var canvas = document.getElementById('canvas', board);
-
-    var board_view = new BoardView(canvas, board);
-    board_view.draw();
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
